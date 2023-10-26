347ba55dd04d65279708899210d53e2e459c8074
wellington <wellington281482@gmail.com>
2023-10-26 13:55:33 -0300

    Arquivo fizbus

---
+++ CursoShell-Blau/fizbuz.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Enunciado
+# -----------------------------------------------------------------------------
+# Passada uma lista de números como argumentos, o script deve imprimir,
+# para cada um deles:
+# 
+# - Se o número for divisível por 3: fiz
+# - Se o número for divisível por 5: buz
+# - Se for divisível por 3 e 5: fizbuz
+# - Se não for divisível por 3 nem por 5: o próprio número
+
