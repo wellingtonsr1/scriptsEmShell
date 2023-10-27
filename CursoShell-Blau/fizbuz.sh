9b3ab23517461a76ecbb305700254cb678434323
wellington <wellington281482@gmail.com>
2023-10-27 03:13:42 -0300

    Resposta do exercício

--- CursoShell-Blau/fizbuz.sh
+++ CursoShell-Blau/fizbuz.sh
@@ -11,3 +11,25 @@
 # - Se for divisível por 3 e 5: fizbuz
 # - Se não for divisível por 3 nem por 5: o próprio número
 
+
+# Verificar se foram passados os argumentos
+if [[ $# -eq 0 ]]; then
+    echo 'Favor, informar a lista de números.'
+    echo "  - Uso: $0 1 2 3...N ou"
+    echo "         $0 {1...N}, Onde <N> é valor final da lista"
+    exit 1
+fi
+
+# Percorrer a lista testando seus valores.
+for i in $@; do       
+    if [[ $i%3 -eq 0 ]] && [[ $i%5 -eq 0 ]]; then
+        echo "$i : fizbuz"       
+    elif [[ $i%3 -eq 0 ]]; then
+        echo "$i : fiz"    
+    elif [[ $i%5 -eq 0 ]]; then
+        echo "$i : buz"
+    else
+        echo $i
+    fi
+done
+
