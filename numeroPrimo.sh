6ecde4d15b1509e90222f54b08c6e070d55666bd
wellington <wellington281482@gmail.com>
2023-10-28 15:00:07 -0300

    Teste

--- numeroPrimo.sh
+++ numeroPrimo.sh
@@ -6,9 +6,9 @@ while :
 do
     clear
     echo '--------------------------------------'
-    echo '    Verifica se um número é primo.'
+    echo '    Verifica se um número é primo.    '
     echo '--------------------------------------'
-    echo -n ' Qual o número? (<q> para sair) : '
+    echo -n ' Qual o número? (<q> para sair) :  '
     read  valor
     echo ' '
 
@@ -17,7 +17,7 @@ do
     primo=true
 
     if [[ $valor -gt 1 ]]; then
-        for ((i=2; i <= ($valor/2); i++)); do
+        for ((i=2; i <= $valor/2; i++)); do
             if [[ $valor%$i -eq 0 ]]; then
                 primo=false
                 break
