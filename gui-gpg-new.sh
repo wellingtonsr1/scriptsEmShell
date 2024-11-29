d476e35ac188bbf373f8480e61c15933c8aaf6ea
wellingtonsr1 <97860975+wellingtonsr1@users.noreply.github.com>
2024-11-29 12:57:18 -0300

    Update gui-gpg-new.sh

--- gui-gpg-new.sh
+++ gui-gpg-new.sh
@@ -9,6 +9,38 @@ ID="Wellington"
 BACKUP_DIR="bkp"
 #---------------------------------------------------------------------------------------------------
 
+:<<'#'
+Descrição:
+----------
+	Este script, denominado GUI-gpg, é uma ferramenta simples e segura para gerenciar senhas utilizando criptografia com GPG (GNU Privacy Guard). 
+	Ele permite ao usuário armazenar informações sensíveis, como login e senha de serviços, em um arquivo criptografado, garantindo sua confidencialidade.
+
+Funcionalidades principais:
+---------------------------
+    Captura de Dados: Solicita ao usuário as informações do serviço, login e senha, com a senha sendo digitada de forma oculta para maior privacidade.
+    Backup Automático: Gera automaticamente um backup do arquivo criptografado existente, organizando-o em um diretório dedicado com nomes que incluem data e hora, evitando sobrescrições.
+    Criptografia e Descriptografia:
+        Descriptografa o arquivo de senhas existente (se houver) para incluir novos dados.
+        Recria o arquivo criptografado atualizado.
+    Segurança de Arquivos:
+        O arquivo de texto temporário utilizado para armazenar senhas antes da criptografia é destruído com o comando shred, garantindo que não possa ser recuperado.
+        As permissões do diretório de backup são configuradas para limitar o acesso apenas ao usuário.
+    Mensagens Informativas: Fornece feedback claro ao usuário sobre as operações realizadas, incluindo sucessos e falhas.
+
+Fluxo de Operação:
+------------------
+    O script solicita ao usuário os dados do serviço (nome, login e senha).
+    Caso o arquivo de senhas criptografado exista:
+        Um backup é gerado.
+        O arquivo é descriptografado e as novas informações são adicionadas.
+    Se o arquivo não existir, um novo é criado.
+    O arquivo atualizado é criptografado novamente e o temporário não criptografado é apagado.
+
+Finalidade:
+-----------
+
+	Ideal para quem deseja gerenciar senhas de maneira simples e segura, sem depender de ferramentas de terceiros ou interfaces complexas. Ele é útil em ambientes de linha de comando e prioriza a privacidade e a segurança dos dados.
+#
 # Função para exibir erros e sair
 error_exit() {
     echo "Erro: $1" >&2
